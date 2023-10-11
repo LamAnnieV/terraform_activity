22c70e7fb105888af6dc9bc655b4c02dc787be12
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-10-11 13:53:00 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+################## D E C L A R E - V A R I A B L E ####################
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "instance_type" {}
+variable "region" {}
+variable "ami" {}
+variable "subnet_id" {}
+variable "vpc_security_group_ids" {}
+variable "deploy_file_name" {}
+variable "key_pair" {}
+variable "aws_instance_name" {}
